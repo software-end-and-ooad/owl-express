cd3e68b50d49b49355be039f80a12adf60f8bd86
Jitiwattana <bbos1@windowslive.com>
2017-10-06 20:08:36 +0700

    ADD send forget password

--- constance/url.ts
+++ constance/url.ts
@@ -3,6 +3,7 @@ const domain =  'http://localhost:3000/api/';
 const api = {
   auth: domain + 'auth',
   register: domain + 'register',
+  forgetpwd: domain + 'forget-password'
 }
 
 const domainProtect = domain + 'user/';
--- src/app/main/forgetpwd/sendforget.component.ts
+++ src/app/main/forgetpwd/sendforget.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
-import { AuthenticationService } from '../../shared';
+import { API } from '../../../../constance/url';
 
 @Component({
   selector: 'main-sendforget',
@@ -14,11 +15,12 @@ export class SendForgetComponent implements OnInit {
   forgetForm: FormGroup;
   formInvalid: string;
   submit: boolean = false;
+  sent: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private authService: AuthenticationService,
+    private http: HttpClient,
   ) {}
 
   ngOnInit() {
@@ -28,39 +30,59 @@ export class SendForgetComponent implements OnInit {
   }
 
   public sendForget(value: any) {
-/*
- *    this.submit = true;
- *    this.authService.login(value.email, value.password)
- *      .subscribe(
- *        res => {
- *          setTimeout(() => {
- *            this.submit = false;
- *            this.formInvalid = null;
- *            this.mainComponent.loginModal.nativeElement.click();
- *          }, 1500);
- *
- *          setTimeout(() => {
- *            this.router.navigateByUrl('dashboard');
- *          }, 2000);
- *        },
- *        err => {
- *          if ( this.router.url == '/login')
- *            this.router.navigateByUrl('login');
- *          else
- *            this.router.navigateByUrl('');
- *
- *          const error = err.error;
- *
- *          setTimeout(() => {
- *            this.submit = false;
- *            if (error.success == false && error.data == 'INVALID_CREDENTIALS')
- *              this.formInvalid = 'อีเมลหรือรหัสผ่านไม่ถูกต้อง';
- *            else
- *              this.formInvalid = 'การเข้าสู่ระบบผิดพลาด กรุณาลองใหม่อีกครั้ง';
- *          }, 1500);
- *        }
- *      );
- */
+    this.submit = true;
+    console.log(value);
+    this.http.post(API.api.forgetpwd, value)
+      .subscribe(
+        (res: any) => {
+          setTimeout(() => {
+            this.sent = true;
+          }, 1500)
+        },
+        (err: any) => {
+          setTimeout(() => {
+            this.sent = false;
+          }, 1500)
+        },
+        () => {
+          setTimeout(() => {
+            this.submit = false;
+          }, 1500)
+        }
+      )
+    /*
+     *    this.submit = true;
+     *    this.authService.login(value.email, value.password)
+     *      .subscribe(
+     *        res => {
+     *          setTimeout(() => {
+     *            this.submit = false;
+     *            this.formInvalid = null;
+     *            this.mainComponent.loginModal.nativeElement.click();
+     *          }, 1500);
+     *
+     *          setTimeout(() => {
+     *            this.router.navigateByUrl('dashboard');
+     *          }, 2000);
+     *        },
+     *        err => {
+     *          if ( this.router.url == '/login')
+     *            this.router.navigateByUrl('login');
+     *          else
+     *            this.router.navigateByUrl('');
+     *
+     *          const error = err.error;
+     *
+     *          setTimeout(() => {
+     *            this.submit = false;
+     *            if (error.success == false && error.data == 'INVALID_CREDENTIALS')
+     *              this.formInvalid = 'อีเมลหรือรหัสผ่านไม่ถูกต้อง';
+     *            else
+     *              this.formInvalid = 'การเข้าสู่ระบบผิดพลาด กรุณาลองใหม่อีกครั้ง';
+     *          }, 1500);
+     *        }
+     *      );
+     */
   }
 
 
